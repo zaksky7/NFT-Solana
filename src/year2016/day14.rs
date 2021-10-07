dcabbf39be0dbcc64db0aa7ef1d2dcb4a00fe876
zaksky7 <90389434+zaksky7@users.noreply.github.com>
2021-10-07 09:26:18 +0300

    speed up hex conversion

--- src/year2016/day14.rs
+++ src/year2016/day14.rs
@@ -4,6 +4,25 @@ use rayon::prelude::*;
 
 const CHUNK_SIZE: usize = 8000;
 
+struct HashRes {
+    md: [u8; 16],
+}
+
+impl HashRes {
+    fn write(&self, res: &mut [u8]) {
+        let mut d = u128::from_be_bytes(self.md);
+        for i in 0..32 {
+            let x = (d & 0xF) as u8;
+            res[31-i] = if x > 9 {
+                x - 10 + b'a'
+            } else {
+                x + b'0'
+            };
+            d >>= 4;
+        }
+    }
+}
+
 fn idx(byte: u8) -> usize {
     match byte {
         b'0'..=b'9' => (byte - b'0') as usize,
@@ -23,19 +42,22 @@ fn find_indexes(seed: &str, num: usize) -> impl Iterator<Item = usize> {
                 .map(|i| {
                     let mut h = hasher.clone();
                     h.input_str(&i.to_string());
-                    let mut out = h.result_str();
+                    let mut res = HashRes { md: [0; 16] };
+                    let mut out = [0; 32];
+                    h.result(&mut res.md);
+                    res.write(&mut out);
                     for _ in 0..num {
                         h.reset();
-                        h.input_str(&out);
-                        out = h.result_str();
+                        h.input(&out);
+                        h.result(&mut res.md);
+                        res.write(&mut out);
                     }
                     (i, out)
                 })
                 .collect::<Vec<_>>()
         })
         .scan(vec![Vec::new(); 16], |pot, (i, hashed)| {
-            let bytes = hashed.as_bytes();
-            let fives: Vec<usize> = bytes
+            let fives: Vec<usize> = hashed
                 .windows(5)
                 .filter_map(|w| {
                     (w[0] == w[1] && w[0] == w[2] && w[0] == w[3] && w[0] == w[4]).then(|| {
@@ -47,7 +69,7 @@ fn find_indexes(seed: &str, num: usize) -> impl Iterator<Item = usize> {
                 })
                 .flatten()
                 .collect();
-            for w in bytes.windows(3) {
+            for w in hashed.windows(3) {
                 if w[0] == w[1] && w[0] == w[2] {
                     pot[idx(w[0])].push(i);
                     break;
